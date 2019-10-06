72a9075d57bf962ab0a87c2c7a35077aa7a11921
vallisweekes <vallis.weekes@gmail.com>
2019-10-06 09:28:56 +0200

    json file updated

--- src/Components/Header/LocationSearch.jsx
+++ src/Components/Header/LocationSearch.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
+import React from "react";
 // import { InputAdornment } from '@material-ui/core/InputAdornment';
 
-import '../../../src/style.css';
+import "../../../src/style.css";
 const LocationSearch = () => {
-	return (
-		<section className=''>
-			<span className='fa-search'></span>
-			<input
-				className='mt-2 search  justify-content-center'
-				type='text'
-				placeholder='Location....'
-			/>
-		</section>
-	);
+  return (
+    <section className="">
+      <input
+        className="mt-2 search  justify-content-center"
+        type="text"
+        placeholder="Location...."
+      />
+    </section>
+  );
 };
 
 export default LocationSearch;
--- src/Components/ResultsContainer/MapContainer/MapContainer.jsx
+++ src/Components/ResultsContainer/MapContainer/MapContainer.jsx
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 
 class MapContainer extends Component {
-	constructor(props) {
-		super(props);
-		console.log(this.props);
-	}
+  constructor(props) {
+    super(props);
+    console.log(this.props);
+  }
 
-	render() {
-		return (
-			<Map
-				google={this.props.google}
-				zoom={15}
-				style={{ width: '45%', position: 'fixed' }}
-				initialCenter={{ lat: this.props.lat, lng: this.props.lon }}
-			>
-				<Marker position={{ lat: this.props.lat, lng: this.props.lon }} />
-			</Map>
-		);
-	}
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={15}
+        style={{ width: "45%" }}
+        initialCenter={{ lat: 51.59351, lng: -0.10257 }}
+      >
+        <Marker position={{ lat: 51.59351, lng: -0.10257 }} />
+      </Map>
+    );
+  }
 }
 
 export default GoogleApiWrapper({
-	apiKey: 'AIzaSyCuURQLKaUsOXYZWDi0ZXyKxCe0Hou48bU&callback'
+  apiKey: "AIzaSyCuURQLKaUsOXYZWDi0ZXyKxCe0Hou48bU&callback"
 })(MapContainer);
