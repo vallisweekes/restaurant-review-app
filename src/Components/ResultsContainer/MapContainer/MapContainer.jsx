80eaafff8261bd53b71c2a7a8829a169a6bbbdf7
Vallis Weekes <vallis.weekes@gmail.com>
2019-10-08 23:06:40 +0100

    added makers

--- src/Components/ResultsContainer/MapContainer/MapContainer.jsx
+++ src/Components/ResultsContainer/MapContainer/MapContainer.jsx
@@ -1,45 +1,47 @@
-import React, { Component } from "react";
-import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
-import RestList from "../../../restList.json";
-class MapContainer extends Component {
-  state = {
-    lat: null,
-    lng: null
-  };
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
+import RestList from '../../../restList.json';
 
-  componentWillMount() {
-    this.setState({
-      lat: this.props.lat,
-      lng: this.props.lng
-    });
-  }
-  render() {
-    console.log(RestList.results);
-    console.log(this.props);
-    return (
-      <Map
-        google={this.props.google}
-        q
-        zoom={15}
-        initialCenter={{
-          lat: this.state.lat,
-          lng: this.state.lng
-        }}
-      >
-        {RestList.results.map(rest => (
-          <Marker
-            key={rest.id}
-            position={{
-              lat: rest.geometry.location.lat,
-              lng: rest.geometry.location.log
-            }}
-          />
-        ))}
-      </Map>
-    );
-  }
+class MapContainer extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			lat: null,
+			lng: null
+		};
+	}
+	UNSAFE_componentWillMount() {
+		this.setState({
+			lat: this.props.lat,
+			lng: this.props.lng
+		});
+	}
+	render() {
+		return (
+			<Map
+				google={this.props.google}
+				zoom={15}
+				style={{ width: '100%', height: '100%', position: 'relative' }}
+				initialCenter={{
+					lat: this.state.lat,
+					lng: this.state.lng
+				}}
+			>
+				{RestList.results.map(rest => (
+					<Marker
+						key={rest.id}
+						name={rest.name}
+						position={{
+							lat: rest.geometry.location.lat,
+							lng: rest.geometry.location.lng
+						}}
+					/>
+				))}
+			</Map>
+		);
+	}
 }
 
 export default GoogleApiWrapper({
-  apiKey: "AIzaSyA8Byy23oFligL0X1_WQYca0ABneIhxOow"
+	apiKey: 'AIzaSyA8Byy23oFligL0X1_WQYca0ABneIhxOow'
 })(MapContainer);
